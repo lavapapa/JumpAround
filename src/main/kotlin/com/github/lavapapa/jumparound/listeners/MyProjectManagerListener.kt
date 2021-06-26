9eb50b75c3df427ac56e3b34c6fb41a2e2d0d2ff
GitHub Action <action@github.com>
2021-06-26 14:12:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/lavapapa/jumparound/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/lavapapa/jumparound/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lavapapa.jumparound.listeners
 
+import com.github.lavapapa.jumparound.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/lavapapa/jumparound/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lavapapa/jumparound/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lavapapa.jumparound.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lavapapa.jumparound.MyBundle
 
 class MyApplicationService {
 
